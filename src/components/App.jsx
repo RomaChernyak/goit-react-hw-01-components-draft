9fa90eb59f21ac34857de19d1b477ba5e3aa2924
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2023-06-28 02:16:46 +0300

    task1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
+import userData from './database/user.json';
+import dataArray from './database/data.json';
+import friendsArray from './database/friends.json';
+import transArray from './database/transactions.json';
+
+import Profile from './Profile';
+
+// const { username, tag, location, avatar, stats } = data;
+// const { followers, views, likes } = stats;
+
+// console.log(dataArray);
+// console.log(friendsArray);
+// console.log(userData);
+// console.log(transArray);
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      {/* Task 1 */}
+      <Profile user={userData} />
+      {/* Task 2 */}
+      Task 2
+      {/* Task 3 */}
+      Task 3
+      {/* Task 4 */}
+      Task 4
+    </>
   );
 };
+
+
+
+// ================================
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
---
+++ src/components/Profile.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+
+const Profile = ({ user }) => {
+    return (
+        <div className="profile">
+            <div className="description">
+                <img
+                    src={user.avatar}
+                    alt={user.username}
+                    className="avatar"
+                />
+                <p className="name">{user.username}</p>
+                <p className="tag">@{user.tag}</p>
+                <p className="location">{user.location}</p>
+            </div>
+
+            <ul className="stats">
+                <li>
+                    <span className="label">Followers</span>
+                    <span className="quantity">{user.stats.followers}</span>
+                </li>
+                <li>
+                    <span className="label">Views</span>
+                    <span className="quantity">{user.stats.views}</span>
+                </li>
+                <li>
+                    <span className="label">Likes</span>
+                    <span className="quantity">{user.stats.likes}</span>
+                </li>
+            </ul>
+        </div>)
+};
+
+// Profile.propTypes = {
+//     username: PropTypes.string.isRequired,
+//     tag: PropTypes.string.isRequired,
+//     location: PropTypes.string.isRequired,
+//     avatar: PropTypes.string.isRequired,
+//     stats: PropTypes.array.isRequired,
+// };
+
+export default Profile;
